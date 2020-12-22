8534348c1fea611de16dc502ee8482e91a591bad
Rheeshabh kumar singh <rishabhs861@gmail.com>
2020-12-22 14:57:58 +0530

    added listView in Numbers.java with array adapter

--- app/src/main/java/com/example/practiceapp1/Numbers.java
+++ app/src/main/java/com/example/practiceapp1/Numbers.java
@@ -1,14 +1,29 @@
 package com.example.practiceapp1;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Numbers extends AppCompatActivity {
+    Integer[] numbers;
+    ListView list;
+    ArrayAdapter<Integer> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        list=(ListView) findViewById(R.id.list);
+        numbers= new Integer[100];
+        for(int i=0;i<100;i++)
+        {
+            numbers[i]=i+1;
+        }
+        arrayAdapter= new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1,numbers);
+        list.setAdapter(arrayAdapter);
     }
 }
